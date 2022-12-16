38767ef9b29393066cb6915eeae34cc2bd1adec0
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-17 03:12:24 +0500

    Exercise_002-4.py Performed

--- Exercise_002-4.py
+++ Exercise_002-4.py
@@ -1,1 +1,16 @@
+# 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях.
 
+import random
+n = int(input('Enter number of elements: '))
+my_list = []
+for i in range(1, n + 1):
+    my_list.append(random.randint(-n, n))
+print(my_list)
+
+position = []
+for i in range(1, 3):
+    position.append(int(input('Enter number position: ')))
+
+result = my_list[position[0]] * my_list[position[1]]
+print(result)
