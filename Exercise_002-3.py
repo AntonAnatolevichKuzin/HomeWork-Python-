6091b9bfbefcb72e73618458c04c17e8a66205c2
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-17 03:09:52 +0500

    Update Exercise_002-3.py

--- Exercise_002-3.py
+++ Exercise_002-3.py
@@ -1,1 +1,10 @@
+# 3 Задайте список из n чисел последовательности (1+ 1/n)^n и выведите на экран их сумму.
 
+n = int(input('Enter number N: '))
+my_dict = {}
+res_sum = 0
+for i in range(1, n + 1):
+    my_dict[i] = round((1 + 1/i) ** i, 3)
+    res_sum += my_dict[i]
+print(f'From n = {n}: {my_dict}')
+print(f'Sum = {res_sum}')
