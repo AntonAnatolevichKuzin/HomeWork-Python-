98d5e9fc37147a27f98ede150109484d16c16111
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-17 03:14:47 +0500

    Exercise_002-5.py Performed

--- Exercise_002-5.py
+++ Exercise_002-5.py
@@ -1,1 +1,16 @@
+# 5. Реализуйте алгоритм перемешивания списка.
 
+import random
+
+my_list = []
+for i in range(1, 8):
+    my_list.append(random.randint(-5, 5))
+print(my_list)
+
+for i in range(0, len(my_list)):
+    if i < len(my_list) - 1:
+        temp = my_list[i]
+        my_list[i] = my_list[i+1]
+        my_list[i+1] = temp
+
+print(my_list)
