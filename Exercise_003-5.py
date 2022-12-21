c15a3199c1b2f352a6d5002f160183ec1f674c52
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-22 02:42:31 +0500

    Exercise_003-5.py performed

--- Exercise_003-5.py
+++ Exercise_003-5.py
@@ -1,1 +1,23 @@
+#  Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+#
+# *Пример:*
+#
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
+num = int(input('Input number: '))
+print(f'для к = {num} список будет выглядеть так: ', end='')
+
+fib_list = []
+for i in range(0, num + 1):
+    if i == 0:
+        fib_list.append(0)
+    elif i == 1 or i == 2:
+        fib_list.append(1)
+    else:
+        fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+
+neg_fib_list = []
+for n in range(1, num + 1):
+    neg_fib_list.insert(0, int(((-1) ** (n + 1)) * fib_list[n]))
+result = neg_fib_list + fib_list
+print(result)
