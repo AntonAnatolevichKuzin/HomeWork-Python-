6a744c5cc78bc0253a7b7f17fff7af4b39f3a72e
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-22 02:39:49 +0500

    Exercise_003-4.py performed

--- Exercise_004-4.py
+++ Exercise_004-4.py
@@ -1,1 +1,16 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#
+# *Пример:*
+#
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+num = int(input('Input number: '))
+print(f'{num} -> ', end='')
+binar = ''
+while num > 0:
+    binar = str(num % 2) + binar
+    num = num // 2
+print(binar)
 
