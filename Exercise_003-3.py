979687a409663c7038088c4bc0a0fb8f19d2488d
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-22 02:37:33 +0500

    Exercise_003-3.py performed

--- Exercise_003-3.py
+++ Exercise_003-3.py
@@ -1,1 +1,18 @@
+#  Задайте список из вещественных чисел. Напишите программу, которая найдёт
+#  разницу между максимальным и минимальным значением дробной части элементов.
+#
+# *Пример:*
+#
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+import random
+
+my_list = []
+for i in range(0, 8):
+    my_list.append(round((random.random() * 10), 3))
+print(f'{my_list} => ', end='')
+
+frac_list = []
+for i in range(len(my_list)):
+    frac_list.append(round((my_list[i] % 1), 3))
+print(max(frac_list) - min(frac_list))
