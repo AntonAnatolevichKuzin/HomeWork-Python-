ab9f6a20fadb1314302f9401a2e09c4b8ad051cd
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-26 21:35:38 +0500

    Exercise_004-1.py performed

--- Exercise_004-1.py
+++ Exercise_004-1.py
@@ -1,1 +1,12 @@
+# Вычислить число c заданной точностью d
+#
+# Пример:
+#
+# - при d = 0.001, π = 3.141    10^{-1} ≤ d ≤10^{-10}
 
+import random
+
+exactnes = input('Задайте точность числа из диапазона 10^{-1} ≤ d ≤10^{-10}: ')
+dig = (abs(str(exactnes).find('.') - len(str(exactnes))) - 1)
+num = float(random.random())
+print(f'при d = {exactnes}, число {num}, примет следующий вид: {round(num, dig)}')
