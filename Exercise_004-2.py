fa3117b51700d043ed742ec7faa1824b13f671a3
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-26 21:37:57 +0500

    Exercise_004-2.py performed

--- Exercise_004-2.py
+++ Exercise_004-2.py
@@ -1,1 +1,14 @@
+#Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+num = int(input('Введите натуральное число: '))
+multipliers = []
+d = 2
+m = num
+while d * d <= m:
+    if m % d == 0:
+        multipliers.append(d)
+        m //= d
+    else:
+        d += 1
+multipliers.append(m)
+print(f'Простые множители числа {num} => {multipliers}')
