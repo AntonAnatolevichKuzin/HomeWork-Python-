d0eb5d2ec96aa55fce08a2c0558131a4a394270f
AntonAK <114100078+AntonAnatolevichKuzin@users.noreply.github.com>
2022-12-26 22:13:31 +0500

    Exercise_004-3.py performed

--- Exercise_004-3.py
+++ Exercise_004-3.py
@@ -1,1 +1,14 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет
+# список неповторяющихся элементов исходной последовательности.
+import random
+
+my_list = []
+for i in range(0, 8):
+    my_list.append(random.randint(0, 10))
+print(my_list)
+corr_list = []
+unic_num = set(my_list)
+for my_list in unic_num:
+    corr_list.append(my_list)
+print(corr_list)
 
